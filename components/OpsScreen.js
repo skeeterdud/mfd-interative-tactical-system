152146cb41adbbdc84ae056b9c7c6590139729d1
skeeterdud <skeeterdud@gmail.com>
2025-12-24 13:15:01 -0600

    Update OpsScreen.js

--- components/OpsScreen.js
+++ components/OpsScreen.js
@@ -1,1 +1,17 @@
+import { UnitsTab } from "./UnitsTab.js";
+
+export function OpsScreen(){
+
+  const container = document.createElement("div");
+  container.className = "screen ops-screen";
+
+  const units = UnitsTab();
+  container.appendChild(units);
+
+  const canvas = document.createElement("div");
+  canvas.className = "canvas";
+  container.appendChild(canvas);
+
+  return container;
+}
 
