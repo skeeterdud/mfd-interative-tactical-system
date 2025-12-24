ab5c0ee1470b3ecf74f401e7cd1bb32f49ec06e8
skeeterdud <skeeterdud@gmail.com>
2025-12-24 13:11:28 -0600

    Update units.js

--- data/units.js
+++ data/units.js
@@ -1,1 +1,7 @@
-
+export const ALL_UNITS = [
+  "Trk 1","Eng 2","Trk 3","Eng 4","Trk 5",
+  "Eng 6","Eng 7","Trk 9","Eng 10","Trk 11",
+  "EMS 1",
+  "Med 1","Med 2","Med 3","Med 5",
+  "Med 6","Med 7","Med 9","Med 10","Med 11"
+];
