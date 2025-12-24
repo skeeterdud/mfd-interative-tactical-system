9b239b57e1a427eb6b50049917a5f7a1c6f18fe0
skeeterdud <skeeterdud@gmail.com>
2025-12-24 13:15:19 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+import { SetupScreen } from "./components/SetupScreen.js";
+import { OpsScreen } from "./components/OpsScreen.js";
+
+const app = document.getElementById("app");
+
+function showSetup(){
+  app.innerHTML = "";
+  app.appendChild(SetupScreen(showOps));
+}
+
+function showOps(){
+  app.innerHTML = "";
+  app.appendChild(OpsScreen());
+}
+
+showSetup();
 
