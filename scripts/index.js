5c040af7c4e1de485c1d656896dbe5c14861a996
skeeterdud <skeeterdud@gmail.com>
2025-12-29 14:56:12 -0600

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,10 @@
+// =========================
+// scripts/index.js
+// =========================
 import { initApp } from "./app.js";
+
+// Helpful smoke test:
+console.log("index.js loaded");
 
 document.addEventListener("DOMContentLoaded", () => {
   initApp("app");
